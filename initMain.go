8156dcbbf513bb6acdabcc1e73a40fa48de3f120
Sergiy Chayka <schayka@gmail.com>
2020-05-07 06:37:50 -0400

    grpc client implementation

--- initMain.go
+++ initMain.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
 	"os"
+	"time"
 
+	sm "github.com/inc4/sm-test/spacemesh"
 	_ "github.com/spacemeshos/go-spacemesh/common/types"
 	_ "github.com/spacemeshos/go-spacemesh/common/util"
+	"google.golang.org/grpc"
 )
 
 var (
@@ -21,8 +25,36 @@ var (
 	flagTest = flag.Bool("test", false, "(option) test")
 )
 
-func test() {
+func echo(s string) (value string, err error) {
+	conn, err := grpc.Dial("localhost:9990", grpc.WithInsecure())
+	if err != nil {
+		return
+	}
+	defer conn.Close()
 
+	c := sm.NewNodeServiceClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	v := sm.SimpleString{Value: s}
+
+	res, err := c.Echo(ctx, &v)
+	if err != nil {
+		return
+	}
+
+	value = res.GetValue()
+
+	return
+}
+
+func test() {
+	v, err := echo("test-ping")
+	if err != nil {
+		fmt.Printf("ERROR: %v\n", err)
+	}
+
+	fmt.Printf("ECHO: %s\n", v)
 }
 
 func addressInfo(address string) {
